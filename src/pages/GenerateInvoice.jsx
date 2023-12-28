c2c24d01aed3a79df3fe5172a4540cbc8708bed0
Enechukwu Chibuike <enechukwuchibuike0@gmail.com>
2023-12-28 11:48:56 +0100

    responsive generateinvoice

--- src/pages/GenerateInvoice.jsx
+++ src/pages/GenerateInvoice.jsx
@@ -22,43 +22,54 @@ export default function GenerateInvoice() {
             <span className="text-primaryBg">Select Identity</span>
           </div>
 
-          <div className="w-[80vw] mt-10 bg-white h-[60vh] flex justify-center items-center">
-            <form className="grid gap-5 w-[50vw]">
-              <strong className="text-black border-b-2 w-full leading-10 border-gray">
+          <div className="w-screen sm:w-[90vw] md:w-[80vw] mt-10 bg-white h-fit py-5 flex justify-center items-center">
+            <form className="grid gap-5 w-[90%] sm:w-3/4 md:w-[50vw]">
+              <strong className="text-black border-b-2 w-full pb-4 border-gray">
                 Kindly fill the following details to proceed
               </strong>
 
               <strong className="text-black">Who are you?</strong>
 
-              <div className="text-black space-x-3">
-                <input
-                  className="radio"
-                  type="radio"
-                  name="whoareyou"
-                  required
-                />
-                <label>Individual</label>
-                <input
-                  className="radio"
-                  type="radio"
-                  name="whoareyou"
-                  required
-                />
-                <label>Corporate</label>
-                <input
-                  className="radio"
-                  type="radio"
-                  name="whoareyou"
-                  required
-                />
-                <label>Federal Agency</label>
-                <input
-                  className="radio"
-                  type="radio"
-                  name="whoareyou"
-                  required
-                />
-                <label>State Agency</label>
+              <div className="text-black flex flex-wrap items-center gap-3">
+                <label className="flex items-center gap-1">
+                  <input
+                    className="radio"
+                    type="radio"
+                    name="whoareyou"
+                    required
+                  />
+                  Individual
+                </label>
+
+                <label className="flex items-center gap-1">
+                  <input
+                    className="radio"
+                    type="radio"
+                    name="whoareyou"
+                    required
+                  />
+                  Corporate
+                </label>
+
+                <label className="flex items-center gap-1">
+                  <input
+                    className="radio"
+                    type="radio"
+                    name="whoareyou"
+                    required
+                  />
+                  Federal Agency
+                </label>
+
+                <label className="flex items-center gap-1">
+                  <input
+                    className="radio"
+                    type="radio"
+                    name="whoareyou"
+                    required
+                  />
+                  State Agency
+                </label>
               </div>
 
               <strong className="text-black">
